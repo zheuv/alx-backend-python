eddc0077f997e6bb9dacbe1604e07c7a91671896
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-02 14:02:54 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module Doc
+"""
+
 from typing import Tuple
 from math import sqrt
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function Doc
+    """
     return (k, sqrt(v))
