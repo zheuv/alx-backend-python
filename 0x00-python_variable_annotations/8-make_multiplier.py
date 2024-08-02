3115bf45fa4418b292fc9648a8d5c7ca13564799
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-02 14:01:46 +0100

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module Doc
+"""
+
+from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Function Doc
+    """
     def multiplier_function(value: float) -> float:
         return value * multiplier
     return multiplier_function
