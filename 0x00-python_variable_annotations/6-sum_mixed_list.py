3748a97d86cefcf8f40529f3157add33964e1e8f
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-02 14:03:50 +0100

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module Doc
+"""
+
 from typing import Union
 
+
 def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+    """
+    Function Doc
+    """
     result = 0
     for i in mxd_lst:
         result += i
