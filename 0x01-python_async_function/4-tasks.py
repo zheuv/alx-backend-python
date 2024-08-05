828ce9a5f7807b862ec2a685752571cb59afdb12
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-08-05 20:44:02 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -13,11 +13,9 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns task_wait_random n times with the specified max_delay.
-    
     Args:
     n (int): The number of times to spawn task_wait_random.
     max_delay (int): The maximum delay for each task_wait_random.
-
     Returns:
     list: A list of all delays (float values) in ascending order.
     """
